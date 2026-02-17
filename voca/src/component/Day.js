0958f957f83ce6433fad9e4c8ff03c35f5e40dd3
박설희 <pshee.dev@gmail.com>
2026-02-18 01:26:52 +0900

    feat: day 삭제 버튼 추가

--- voca/src/component/Day.js
+++ voca/src/component/Day.js
@@ -1,14 +1,38 @@
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import Word from './Word';
 import useFetch from '../hooks/useFetch';
 
 export default function Day() {
     const { day } = useParams();
     const words = useFetch(`http://localhost:3001/words?day=${day}`)
+    const curDay = useFetch("http://localhost:3001/days").find(curDay => {
+        return curDay.day === Number(day)
+    });
+    const navigate = useNavigate();
     
+    function dayDelete() {
+        if(window.confirm("삭제 하시겠습니까?")) {
+            fetch(`http://localhost:3001/days/${curDay?.id}`, {
+                method: "DELETE",
+            })
+            .then(res => {
+                if (res.ok) {
+                    navigate("/");
+                }
+            });
+        }
+    }
+
     return (
-        <>
-            <h2>Day {day}</h2>
+        <>  
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center'
+            }}>
+                <h2>Day {day}</h2>
+                <button onClick={dayDelete} className="btn_del">Day 삭제</button>
+            </div>
             {words.length === 0 && <span>Loading...</span>}
             <table>
                 <tbody>
